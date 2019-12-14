3dbc947032e645acf9b27c5d7aa4a44cfee824ee
arlogoldman <54850964+arlogoldman@users.noreply.github.com>
2019-12-14 15:12:28 -0500

    Add files via upload

---
+++ a4.js
@@ -0,0 +1,37 @@
+var move1 = document.getElementById('top1');
+
+move1.addEventListener("mouseover", SquareAnimation);
+function animation() {
+
+}
+move1.addEventListener("mouseup", RemoveAddClass);
+function RemoveAddClass() {
+
+
+}
+
+
+
+
+var move2 = document.getElementById('top2');
+
+move2.addEventListener("mouseover", animation);
+function animation() {
+
+}
+move2.addEventListener("mouseup", RemoveAddClass);
+function RemoveAddClass() {
+
+}
+
+
+
+var move3 = document.getElementById('e3');
+move3.addEventListener("mouseover", animation);
+function animation() {
+}
+move3.addEventListener("mouseup", RemoveAddClass);
+function RemoveAddClass() {
+
+
+}
